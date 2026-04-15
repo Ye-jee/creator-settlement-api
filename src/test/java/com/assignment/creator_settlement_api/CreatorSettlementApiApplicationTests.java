118cfb6c583f7a49e0f0f8dcb651d8196120c227
Ye-jee <yejee0408@gmail.com>
2026-04-15 20:16:06 +0900

    init: initialize project

---
+++ src/test/java/com/assignment/creator_settlement_api/CreatorSettlementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.creator_settlement_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorSettlementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
